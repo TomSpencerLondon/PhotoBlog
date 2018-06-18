9bb7792a72472ab77848b6b3220be644e3464958
tomspencerlondon <tomspencer_uk@yahoo.co.uk>
2018-06-18 23:30:30 +0100

    Add rspec and tests for user validation

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  # For testing
+
+  gem 'rspec-rails'
 end
 
 group :development do
@@ -67,3 +70,5 @@ gem 'mini_magick'
 gem 'fog'
 gem 'figaro'
 gem 'unf'
+
+
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  before(:each) do 
+    @user = User.create!(name: "test name", email: "test@test.co.uk", 
+                        password: "testing123", )
+  end 
+
+  describe "creation" do 
+    it 'should have one item created after being created' do 
+      expect(User.all.count).to eq(1) 
+    end 
+  end 
+
+  describe "validations" do 
+    it 'should not let a user be created without an email address' do 
+      @user.email = nil 
+      expect(@user).to_not be_valid
+    end
+    
+    it 'should not let a user be created without a password' do 
+      @user.password = nil 
+      expect(@user).to_not be_valid
+    end 
+
+  end 
+
+end
